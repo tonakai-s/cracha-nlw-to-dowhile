1f07837c9050805597424f7c4337bf2cf5725759
Renan Dorneles Schuquel <43563007+RenanDrnls@users.noreply.github.com>
2021-10-21 23:54:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const userSocialMedias = {
+    github: 'renandrnls',
+    linkedin: 'in/renandrnls/',
+    facebook: 'profile.php?id=100010200602377',
+    instagram: 'renan_dorneles.jpg/',
+    twitter: 'RenanDorneles5'
+}
+
+function changeSocialMediaLinks(){
+    for(let li of socialLinks.children){
+        const social = li.getAttribute('class')
+        li.children[0].href = `https://www.${social}.com/${userSocialMedias[social]}`
+    }
+}
+changeSocialMediaLinks()
+
+function getGutHubProfileInfos() {
+    const url = `https://api.github.com/users/${userSocialMedias.github}`
+
+    fetch(url).then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.url
+        userPhoto.src = data.avatar_url
+        userNickname.textContent = data.login
+    })
+}
+getGutHubProfileInfos()
